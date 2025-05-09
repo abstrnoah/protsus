aeb7f6d47f84b2ad1ec1ba316f92217495200b6a
Noah D. Ortiz <abstrnoah@brumal.net>
2025-05-09 19:32:00 -0400

    improve test.sh

--- test.sh
+++ test.sh
@@ -2,19 +2,23 @@
 
 . ./lib.sh
 
-target="./protsus-test-target"
+target="./test-target"
 passphrase="1nf3cted"
-cleartext="Super secret stuff"
-f="testfile"
 pf="$(path_to_protected_path "$f")"
 
-clearimage_f=$(readlink -f "./secret-image.png")
+testimage=$(readlink -f "./secret-image.png")
 
 teststatus=PASSED
 
-# teardown() {
-# }
-# trap teardown EXIT INT QUIT TERM
+
+temp="$(mktemp -d \
+        || oops "unable to create temporary build directory")"
+
+teardown() {
+    echo "RESULTS: $teststatus"
+    rm -rf "$temp"
+}
+trap teardown EXIT INT QUIT TERM
 
 do_check() {
     local name="$1"
@@ -33,78 +37,76 @@ unsafe_decrypt() {
     || gpg --decrypt "$pf" 2>/dev/null
 }
 
+diff() {
+    command diff -q "$@" >/dev/null
+}
+
 check_protected_file() {
     local pf="$1"
     shift
-    local cleartext="$1"
-    diff -q <(unsafe_decrypt "$pf") <(echo "$cleartext") >/dev/null
+    local cf="$1"
+    diff <(unsafe_decrypt "$pf") "$cf"
 }
 
 check_lock() {
     local f="$1"
     shift
-    local cleartext="$1"
+    local cf="$1"
     test -f "$f" \
-    && ! diff -q <(echo "$cleartext") "$f" >/dev/null \
-    && diff -q <(echo "$tombstone") "$f" >/dev/null \
-    && is_locked "$f" >/dev/null
+    && ! diff "$cf" "$f" \
+    && diff <(echo "$tombstone") "$f" \
+    && is_locked "$f"
 }
 
-check_lock_idempotent() {
+check_idempotent() {
+    local c="$1"
+    shift
     local f="$1"
     local pf="$(path_to_protected_path "$f")"
-    lock "$f" &>/dev/null
-    local f_contents="$(cat "$f")"
-    local pf_contents="$(cat "$pf")"
-    lock "$f" &>/dev/null
-    lock "$f" &>/dev/null
-    diff -q <(lock "$f") <(echo "$0: Already locked: $f")
-    diff -q <(echo "$f_contents") "$f"
-    diff -q <(echo "$pf_contents") "$pf"
-}
-
-check_unlock_idempotent() {
-    local f="$1"
-    local pf="$(path_to_protected_path "$f")"
-    unlock "$f" &>/dev/null
-    local f_contents="$(cat "$f")"
-    local pf_contents="$(cat "$pf")"
-    unlock "$f" &>/dev/null
-    unlock "$f" &>/dev/null
-    diff -q <(unlock "$f") <(echo "$0: Already unlocked: $f")
-    diff -q <(echo "$f_contents") "$f"
-    diff -q <(echo "$pf_contents") "$pf"
+    "$c" "$f" &>/dev/null
+    cp "$f" "$f.old"
+    cp "$pf" "$pf.old"
+    "$c" "$f" &>/dev/null
+    "$c" "$f" &>/dev/null
+    diff "$"$c"d" "$f"
+    diff "$pf.old" "$pf"
 }
 
 check_unlock() {
     local f="$1"
     shift
-    local cleartext="$1"
-    diff -q <(echo "$cleartext") "$f" >/dev/null
+    local cf="$1"
+    diff "$cf" "$f" \
+    && is_unlocked "$f"
 }
 
 rm -rf "$target"
 mkdir -p "$target"
 cd "$target"
 
+cf="$temp/cleartext"
+f="regular-file"
+echo "$cleartext" > "$cf"
 echo "$cleartext" > "$f"
 protect "$f"
-do_check "protect" check_protected_file "$(path_to_protected_path "$f")" "$cleartext"
-do_check "lock" check_lock "$f" "$cleartext"
-do_check "lock idempotent" check_lock_idempotent "$f"
+do_check "protect" check_protected_file "$(path_to_protected_path "$f")" "$cf"
+do_check "lock" check_lock "$f" "$cf"
+do_check "lock idempotent" check_idempotent lock "$f"
 unlock "$f"
-do_check "unlock" check_unlock "$f" "$cleartext"
-do_check "unlock idempotent" check_unlock_idempotent "$f"
+do_check "unlock" check_unlock "$f" "$cf"
+do_check "unlock idempotent" check_idempotent unlock "$f"
 lock "$f"
-do_check "lock" check_lock "$f" "$cleartext"
-do_check "lock idempotent" check_lock_idempotent "$f"
-rm "$f" "$pf"
+do_check "lock" check_lock "$f" "$cf"
+do_check "lock idempotent" check_idempotent lock "$f"
 
-cp "$clearimage_f" "$f"
-protect "$f"
-open "$f"
-do_check "image unlocked after open" is_unlocked "$f"
-lock "$f"
-do_check "image locked after open+lock" is_locked "$f"
+# cf="$testimage"
+# f="image.png"
+# cp "$testimage" "$f"
+# protect "$f"
+# do_check "protect" check_protected_file "$(path_to_protected_path "$f")" "$cf"
+# do_check "lock" check_lock "$f" "$cf"
+# open "$f"
+# do_check "image unlocked after open" is_unlocked "$f"
+# lock "$f"
+# do_check "image locked after open+lock" is_locked "$f"
 
-echo "RESULTS: $teststatus"
